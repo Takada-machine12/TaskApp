414a189e4472f24b9d072c226f73c7d464dd4d75
Takada-machine12 <147371960+Takada-machine12@users.noreply.github.com>
2026-03-30 21:57:41 +0900

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,17 +1,10 @@
 <?php
 define('SERVER_NAME', 'タスク管理');
 define('SERVICE_SHORT_NAME', 'TaskApp');
-define('SITE_URL', 'http://goldcivet74.sakura.ne.jp/TaskApp/web/');
-define('DB_HOST', 'mysql3112.db.sakura.ne.jp');
-define('DB_USER', 'goldcivet74_sample01');
-define('DB_PASSWORD', 'takada1278');
-define('DB_NAME', 'goldcivet74_sample01');
-define('ADMIN_MAIL_ADDRESS', 'y.takada@eclat-assist.jp');
-
-//ローカル環境
-//define('SITE_URL', 'http://localhost/develop/TaskApp/web/');
-//define('DB_HOST', 'localhost');
-//define('DB_USER', 'root');
-//define('DB_PASSWORD', '');
-//define('DB_NAME', 'taskmanager');
+define('SITE_URL', 'サイトURL');
+define('DB_HOST', 'ホスト名');
+define('DB_USER', 'ユーザ名');
+define('DB_PASSWORD', 'パスワード');
+define('DB_NAME', 'DB名');
+define('ADMIN_MAIL_ADDRESS', '管理者メールアドレス');
 ?>
