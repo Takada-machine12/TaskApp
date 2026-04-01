8863ac8433016030bda087db92de90f83fe08fe5
TK <takada311@gmail.com>
2026-04-01 19:37:15 +0900

    DB削除処理を追加。

--- web/logout.php
+++ web/logout.php
@@ -19,6 +19,7 @@ if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time()-86400, '/');
 }
 session_destroy();  //セッション自体を破棄する。
+unset($pdo);
 
 //TOPページへリダイレクト
 header('Location:'.SITE_URL.'index.php');
